16fec31fb50f659b9b0a5dea9ceaeca29faac5d6
blac <badejo.semilore.7@gmail.com>
2023-08-23 12:30:08 +0100

    created user and file models

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,6 +2,6 @@ import { Sequelize } from "sequelize";
 
 const db = new Sequelize(
   "postgres://risevestdb_user:1ItPF2a4ZcueDkfguj1aCGHfLvVXmVwf@dpg-cjij51ocfp5c738hovc0-a.oregon-postgres.render.com/risevestdb?ssl=true"
-); // Example for postgres
+);
 
 export default db;
