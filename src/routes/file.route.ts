87d1c584fb110007c204b11d9e700ec22a488380
blac <badejo.semilore.7@gmail.com>
2023-08-25 10:26:14 +0100

    added download route

--- src/routes/file.route.ts
+++ src/routes/file.route.ts
@@ -1,6 +1,6 @@
 import express, { Router } from "express";
 import { protect, restrictTo } from "../controllers/auth.controller";
-import { uploadFile } from "../controllers/file.controller";
+import { uploadFile, downloadFile } from "../controllers/file.controller";
 import { uploadSingle } from "../middleware/multer";
 
 const router: Router = express.Router();
@@ -8,5 +8,6 @@ const router: Router = express.Router();
 router.use(protect);
 
 router.post("/upload", uploadSingle, uploadFile);
+router.get("/download/:fileName", downloadFile);
 
 export default router;
